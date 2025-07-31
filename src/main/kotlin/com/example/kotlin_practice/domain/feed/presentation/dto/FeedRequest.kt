64da93dcee2796b927fefaad88379d7aa19c1e43
박태수 <taesupark0504@gmail.com>
2025-07-31 22:06:26 +0900

    feat :: feedRequest

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/feed/presentation/dto/FeedRequest.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin_practice.domain.feed.presentation.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class FeedRequest(
+    @field:NotBlank(message = "제목을 입력하세요")
+    @field:Size(max = 20, message = "제목은 20자 이내로 작성해주세요")
+    var title: String,
+
+    @field:NotBlank(message = "내용을 입력하세요")
+    @field:Size(max = 200, message = "200자 이내로 작성해주세요")
+    var content: String
+)
