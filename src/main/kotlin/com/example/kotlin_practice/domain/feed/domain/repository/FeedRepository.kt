2dd19e0a00b05cbfeeef0adacca0f7ec3d67f4b3
박태수 <taesupark0504@gmail.com>
2025-07-31 22:06:10 +0900

    feat :: feedRepository

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/feed/domain/repository/FeedRepository.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin_practice.domain.feed.domain.repository
+
+import com.example.kotlin_practice.domain.feed.domain.Feed
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface FeedRepository : JpaRepository<Feed, Long> {
+}
