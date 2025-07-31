a35ef5d18d81c8735df49720c782944656f460ca
박태수 <taesupark0504@gmail.com>
2025-07-31 22:06:20 +0900

    feat :: createFeedService

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/feed/service/CreateFeedService.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin_practice.domain.feed.service
+
+import com.example.kotlin_practice.domain.feed.domain.Feed
+import com.example.kotlin_practice.domain.feed.domain.repository.FeedRepository
+import com.example.kotlin_practice.domain.feed.presentation.dto.FeedRequest
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class CreateFeedService(
+    private val feedRepository: FeedRepository
+) {
+
+    @Transactional
+    fun execute(request: FeedRequest) {
+        feedRepository.save(
+            Feed(
+                title = request.title,
+                content = request.content
+            )
+        )
+    }
+}
