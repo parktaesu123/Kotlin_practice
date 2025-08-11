c0e86b53b76e7cb6f04692e278e954c76317f0d3
박태수 <taesupark0504@gmail.com>
2025-08-12 00:28:20 +0900

    feat :: queryFeed API

--- src/main/kotlin/com/example/kotlin_practice/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/feed/presentation/FeedController.kt
@@ -1,9 +1,13 @@
 package com.example.kotlin_practice.domain.feed.presentation
 
-import com.example.kotlin_practice.domain.feed.presentation.dto.FeedRequest
+import com.example.kotlin_practice.domain.feed.presentation.dto.request.FeedRequest
+import com.example.kotlin_practice.domain.feed.presentation.dto.response.FeedResponse
 import com.example.kotlin_practice.domain.feed.service.CreateFeedService
+import com.example.kotlin_practice.domain.feed.service.QueryFeedService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,11 +17,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/feed")
 class FeedController(
-    private val createFeedService: CreateFeedService
+    private val createFeedService: CreateFeedService,
+    private val queryFeedService: QueryFeedService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createFeed(@RequestBody @Valid request: FeedRequest) {
         createFeedService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    fun queryFeed(@PathVariable id: Long): FeedResponse {
+        return queryFeedService.execute(id)
     }
 }
