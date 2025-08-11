8fd78b5639d676518718f70c55fc144e1f71c665
박태수 <taesupark0504@gmail.com>
2025-08-12 00:31:00 +0900

    refactor :: json 객체 형식으로 변경

--- src/main/kotlin/com/example/kotlin_practice/global/error/exeption/ErrorCode.kt
+++ src/main/kotlin/com/example/kotlin_practice/global/error/exeption/ErrorCode.kt
@@ -1,5 +1,8 @@
 package com.example.kotlin_practice.global.error.exeption
 
+import com.fasterxml.jackson.annotation.JsonFormat
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 enum class ErrorCode(
     val status: Int,
     val message: String
