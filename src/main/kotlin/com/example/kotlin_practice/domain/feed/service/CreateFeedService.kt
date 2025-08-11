1459ddb299765cb7654c33ab2f6ec6fc793882ed
박태수 <taesupark0504@gmail.com>
2025-08-12 00:25:45 +0900

    refactor :: request 경로 변경

--- src/main/kotlin/com/example/kotlin_practice/domain/feed/service/CreateFeedService.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/feed/service/CreateFeedService.kt
@@ -2,7 +2,7 @@ package com.example.kotlin_practice.domain.feed.service
 
 import com.example.kotlin_practice.domain.feed.domain.Feed
 import com.example.kotlin_practice.domain.feed.domain.repository.FeedRepository
-import com.example.kotlin_practice.domain.feed.presentation.dto.FeedRequest
+import com.example.kotlin_practice.domain.feed.presentation.dto.request.FeedRequest
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
