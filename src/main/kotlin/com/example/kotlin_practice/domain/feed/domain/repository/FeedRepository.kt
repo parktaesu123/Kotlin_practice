5d1a50393b1197a2b47cd941a60ad56558b760af
박태수 <taesupark0504@gmail.com>
2025-08-12 04:42:36 +0900

    chore :: 형식

--- src/main/kotlin/com/example/kotlin_practice/domain/feed/domain/repository/FeedRepository.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/feed/domain/repository/FeedRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FeedRepository : JpaRepository<Feed, Long> {
+interface FeedRepository: JpaRepository<Feed, Long> {
 }
