92a61445c528d05ccffa4ed84ec93ea88527afed
박태수 <taesupark0504@gmail.com>
2025-08-13 18:42:10 +0900

    refactor :: JpaRepository를 상속받도록 변경

--- src/main/kotlin/com/example/kotlin_practice/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/user/domain/repository/UserRepository.kt
@@ -1,9 +1,9 @@
 package com.example.kotlin_practice.domain.user.domain.repository
 
 import com.example.kotlin_practice.domain.user.domain.User
-import org.springframework.data.repository.CrudRepository
-import java.util.Optional
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface UserRepository: CrudRepository<User, Long> {
+interface UserRepository: JpaRepository<User, Long> {
     fun findByAccountId(accountId: String): Optional<User>
 }
