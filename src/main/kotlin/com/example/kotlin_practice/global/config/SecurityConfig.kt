345a047e0e7a68a972954221a2cb31b755b4e2fa
박태수 <taesupark0504@gmail.com>
2025-08-13 18:43:00 +0900

    feat :: passEncoder등록

---
+++ src/main/kotlin/com/example/kotlin_practice/global/config/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_practice.global.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val objectMapper: ObjectMapper
+) {
+    @Bean
+    fun passwordEncorder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}
