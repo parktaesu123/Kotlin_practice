c02903c07cb8dac31707c0cfbcd6032e467b97b4
박태수 <taesupark0504@gmail.com>
2025-08-13 19:14:06 +0900

    feat :: findByToken

--- src/main/kotlin/com/example/kotlin_practice/domain/auth/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -2,6 +2,8 @@ package com.example.kotlin_practice.domain.auth.domain.repository
 
 import com.example.kotlin_practice.domain.auth.domain.RefreshToken
 import org.springframework.data.repository.CrudRepository
+import java.util.Optional
 
 interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
+    fun findByToken(token: String): Optional<RefreshToken>
 }
