9ccfe22203e7fa4e6b8b9e7b92eaa49585575e83
박태수 <taesupark0504@gmail.com>
2025-08-13 19:14:39 +0900

    feat :: Reissue API

--- src/main/kotlin/com/example/kotlin_practice/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/auth/presentation/AuthController.kt
@@ -1,12 +1,15 @@
 package com.example.kotlin_practice.domain.auth.presentation
 
 import com.example.kotlin_practice.domain.auth.presentation.dto.request.LoginRequest
+import com.example.kotlin_practice.domain.auth.presentation.dto.request.RefreshTokenRequest
 import com.example.kotlin_practice.domain.auth.presentation.dto.request.SignupRequest
 import com.example.kotlin_practice.domain.auth.presentation.dto.response.TokenResponse
 import com.example.kotlin_practice.domain.auth.service.LoginService
+import com.example.kotlin_practice.domain.auth.service.ReissueService
 import com.example.kotlin_practice.domain.auth.service.SignupService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,7 +20,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/auth")
 class AuthController(
     private val signupService: SignupService,
-    private val loginService: LoginService
+    private val loginService: LoginService,
+    private  val reissueService: ReissueService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
@@ -29,5 +33,11 @@ class AuthController(
     @PostMapping("/login")
     fun login(@RequestBody @Valid request: LoginRequest): TokenResponse {
         return loginService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/reissue")
+    fun reissue(@RequestBody @Valid request: RefreshTokenRequest): TokenResponse {
+        return reissueService.execute(request)
     }
 }
