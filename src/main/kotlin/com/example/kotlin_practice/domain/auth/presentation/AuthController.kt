35e599c4ae7cc71d6b73435d40e97b51626a5fb2
박태수 <taesupark0504@gmail.com>
2025-08-13 19:00:26 +0900

    feat :: login API

--- src/main/kotlin/com/example/kotlin_practice/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/auth/presentation/AuthController.kt
@@ -1,6 +1,9 @@
 package com.example.kotlin_practice.domain.auth.presentation
 
+import com.example.kotlin_practice.domain.auth.presentation.dto.request.LoginRequest
 import com.example.kotlin_practice.domain.auth.presentation.dto.request.SignupRequest
+import com.example.kotlin_practice.domain.auth.presentation.dto.response.TokenResponse
+import com.example.kotlin_practice.domain.auth.service.LoginService
 import com.example.kotlin_practice.domain.auth.service.SignupService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -13,11 +16,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/auth")
 class AuthController(
-    private val signupService: SignupService
+    private val signupService: SignupService,
+    private val loginService: LoginService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     fun signup(@RequestBody @Valid request: SignupRequest) {
         signupService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/login")
+    fun login(@RequestBody @Valid request: LoginRequest): TokenResponse {
+        return loginService.execute(request)
     }
 }
