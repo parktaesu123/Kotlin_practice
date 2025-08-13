e7a9331d9d555d69fa31381b2fa689d620c1cfe8
박태수 <taesupark0504@gmail.com>
2025-08-13 18:42:36 +0900

    feat :: signupService

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/auth/service/SignupService.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin_practice.domain.auth.service
+
+import com.example.kotlin_practice.domain.auth.presentation.dto.request.SignupRequest
+import com.example.kotlin_practice.domain.user.domain.User
+import com.example.kotlin_practice.domain.user.domain.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class SignupService(
+    val userRepository: UserRepository,
+    val passwordEncoder: PasswordEncoder
+) {
+    fun execute(request: SignupRequest) {
+        userRepository.save(
+            User(
+                id = 0,
+                accountId = request.accountId,
+                password = passwordEncoder.encode(request.password),
+                userName = request.userName
+            )
+        )
+    }
+}
